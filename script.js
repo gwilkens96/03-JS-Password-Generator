86bec28db40656109effcac6c5548341af3de13d
GriffinWilkens96 <gwilkens1234@gmail.com>
2021-09-16 23:16:33 -0400

    finished coding webpage and updated readme with description and image

--- script.js
+++ script.js
@@ -4,43 +4,66 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   console.log("Button is clicked.")
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-Array.from(lowerCase);
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-Array.from(upperCase);
-var numberCharacters = "0123456789";
-Array.from(numberCharacters)
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-Array.from(specialCharacters)
+var lowerCase = Array.from('abcdefghijklmnopqrstuvwxyz');
+var upperCase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var numberCharacters = Array.from("0123456789");
+var specialCharacters = Array.from("!#$%&*+-./:<=>?@[]^_`{|}~");
+var tempArray = [];
+var finalPassword = "";
 
 
   
  
  var characterLength = window.prompt("How many characters (8-128) will your password be?");
-  if (characterLength < 7 || characterLength > 129) {
+  if (characterLength < 8 || characterLength > 128) {
     window.alert("Please select a length between 8 and 128 characters.");
     return;
-  } else {
-    lowerCase = window.confirm("Would you like to include lowercase letters?");
-    upperCase = window.confirm("Would you like to include uppercase letters?");
-    numberCharacters = window.confirm("Would you like to include numbers?");
-    specialCharacters = window.confirm("Would you like to include special characters?");
+  } else if (characterLength > 7 && characterLength < 129) {
     
-    };
+  } else {
+    window.alert("Please select a valid number.")
+    return;
+  
+  } 
+    
+    var lowerCaseW = window.confirm("Would you like to include lowercase letters?");
+    var upperCaseW = window.confirm("Would you like to include uppercase letters?");
+    var numberCharactersW = window.confirm("Would you like to include numbers?");
+    var specialCharactersW = window.confirm("Would you like to include special characters?");
+    
+  
 
   if (!lowerCase && !upperCase && !numberCharacters && !specialCharacters) {
     window.alert("Please select at least one option.");
+
   
   }
 
-  
+if (lowerCaseW) {
+  tempArray = tempArray.concat(lowerCase);
+}
+
+if (upperCaseW) {
+  tempArray = tempArray.concat(upperCase);
+}
+
+if (numberCharactersW) {
+  tempArray = tempArray.concat(numberCharacters);
+}
+
+if (specialCharactersW) {
+  tempArray = tempArray.concat(specialCharacters);
+}
+
+for(var i = 0; i <= characterLength; i++) {
+
+  finalPassword += tempArray[Math.floor(Math.random() * tempArray.length)];
+
+}
 
 
-  
 
-  
-
-  return
+return finalPassword;
 }
 // Write password to the #password input
 function writePassword() {
